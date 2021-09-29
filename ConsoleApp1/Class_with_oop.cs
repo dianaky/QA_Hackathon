fbeae4f38c9fb08bcae673ce0781983c8a8f8133
Diana Kyryna <d.kyryna@student.csn.khai.edu>
2021-09-29 15:52:36 +0300

    1, 2, 6 tasks

---
+++ ConsoleApp1/Class_with_oop.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class animals //animals
+    {
+        string name;
+        public animals(string name)
+        {
+            this.name = name;
+        }
+    }
+    class unicellular : animals //одколеточные 
+    {
+        bool movement;
+        public unicellular(string name, bool movement) : base(name)
+        {
+            this.movement = movement; }
+    }
+    class multicellular : animals //многоклеточные
+    {
+        bool in_woter;
+        public multicellular(string name, bool in_woter) : base(name)
+        {
+            this.in_woter = in_woter;
+        }
+
+    }
+    class vertebrates : multicellular //позвоночные
+    {
+        bool fish;
+        public vertebrates(string name, bool in_woter,bool fish) : base(name,in_woter)
+        {
+            this.fish = fish; 
+        }
+    }
+    class invertebrates : multicellular //безпозвоночные
+    {
+        bool ground;
+        public invertebrates(string name, bool in_woter, bool ground) : base(name, in_woter)
+        {
+            this.ground = ground;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1.symma 2.rand numbers 3.generate numbers and write\nВаш выбор:");
+            string choose = Console.ReadLine();
+            switch (choose)
+            {
+                case "1":
+                    symma();
+                    Console.ReadKey();
+                    break;
+                case "2":
+                    double R = RND();
+                    Console.Write(R);
+                    Console.Write(" ");
+                    break;
+                case "3": //нужно переделать
+                    for (int i = 0; i < 100; i++) 
+                    {
+                        double R2 = RND();
+                        Console.Write(R2);
+                        Console.Write(" ");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+
+        }
+        static void symma()
+        {
+            try
+            {
+                Console.WriteLine("Введите первое число:");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите второе число:");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"   Сумма = {a + b}");
+            }
+            catch
+            {
+                Console.WriteLine("Error input");
+            }
+        }
+        public static byte RND()
+        {
+            RNGCryptoServiceProvider Rand = new RNGCryptoServiceProvider();
+            byte[] randomNumber = new byte[1];
+            Rand.GetBytes(randomNumber);
+            return (randomNumber[0]);
+        }
+    }
+}
